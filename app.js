a076a31df31c728d56f538d0f963eaa8d49e5e1f
Matacos3 <acosta.garcia.mateo@gmail.com>
2024-02-15 18:11:15 +0100

    modification de app.js pour ajouter cors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./models/connection');
 
+const cors = require("cors")
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
